61d98624861d0e533607083815f3edeaa510247d
Tony Araújo <tnr.arauo@gmail.com>
2021-12-26 15:46:22 -0300

    first commit

---
+++ src/main/kotlin/com/tony/FundamentosKotlinAluraApplication.kt
@@ -0,0 +1,11 @@
+package com.tony
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FundamentosKotlinAluraApplication
+
+fun main(args: Array<String>) {
+	runApplication<FundamentosKotlinAluraApplication>(*args)
+}
